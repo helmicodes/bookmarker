803057717571c2ade85186217dda882a43b230bb
helmicodes <helmicodes@gmail.com>
2023-02-25 22:50:33 +0800

    add link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,4 @@
+require "mechanize"
+
 module ApplicationHelper
 end
---
+++ app/helpers/links_helper.rb
@@ -0,0 +1,9 @@
+module LinksHelper
+  def link_title(url)
+    Mechanize.new.get(url).title
+  end
+
+  def link_image_url(url)
+    Mechanize.new.get(url).at('meta[property="og:image"]')&.[]('content')
+  end
+end
