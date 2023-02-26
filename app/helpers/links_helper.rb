4d9580bc9904e0c0f3d04365d6170dbd73153914
helmicodes <helmicodes@gmail.com>
2023-02-26 22:25:13 +0800

    add link styling

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -6,4 +6,8 @@ module LinksHelper
   def link_image_url(url)
     Mechanize.new.get(url).at('meta[property="og:image"]')&.[]('content')
   end
+
+  def link_host(url)
+    Mechanize.new.get(url).uri.host
+  end
 end
